026533bb58dd8e8e38866e4e46eacc0f33c46d78
poliHorser <134537683+poliHorser@users.noreply.github.com>
2023-10-20 21:21:27 +0300

    go

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,39 +1,48 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
-const email = form.querySelector('input[name="email"]');
-const message = form.querySelector('textarea[name="message"]');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+const sendButton = form.querySelector('button[type="submit"]');
 
+// Функція для оновлення сховища
 const updateLocalStorage = throttle(() => {
     const feedbackState = {
-        email: email.value,
-        message: message.value,
+        email: emailInput.value,
+        message: messageInput.value,
     };
     localStorage.setItem('feedback-form-state', JSON.stringify(feedbackState));
-}, 500); 
+}, 500); // Оновлювати не частіше одного разу в 500 мілісекунд
 
+// Функція для завантаження даних зі сховища
 const loadFromLocalStorage = () => {
-    const storedState = localStorage.getItem('feedback-form-state');
-    if (storedState) {
-        const feedbackState = JSON.parse(storedState);
-        email.value = feedbackState.email;
-        message.value = feedbackState.message;
+    const savedState = localStorage.getItem('feedback-form-state');
+    if (savedState) {
+        const parsedState = JSON.parse(savedState);
+        emailInput.value = parsedState.email;
+        messageInput.value = parsedState.message;
+    } else {
+        emailInput.value = '';
+        messageInput.value = '';
     }
-};
+}
 
-form.addEventListener('input', updateLocalStorage);
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    console.log({
-        email: email.value,
-        message: message.value
-    });
-    // localStorage.removeItem('feedback-form-state');
-    loadFromLocalStorage();
-    
+// Очищення сховища та полів форми при відправці
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    const formState = {
+        email: emailInput.value,
+        message: messageInput.value,
+    };
+    console.log(formState);
+    localStorage.removeItem('feedback-form-state');
+    emailInput.value = '';
+    messageInput.value = '';
+    sendButton.disabled = true;
 });
 
+// Додайте обробники подій для відслідковування вводу
+form.addEventListener('input', updateLocalStorage);
 
-
-
-
+// Завантажте дані зі сховища при завантаженні сторінки
+loadFromLocalStorage();
